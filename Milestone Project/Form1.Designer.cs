f5c6ed0977025f40990b2af60725907ca1a7328e
DavidP-GCU <114124010+DavidP-GCU@users.noreply.github.com>
2022-09-23 10:05:21 -0600

    Add files via upload

---
+++ Milestone Project/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace Milestone_Project
+{
+    partial class milestoneProject
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.viewButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addBananas = new System.Windows.Forms.Button();
+            this.addApples = new System.Windows.Forms.Button();
+            this.addOranges = new System.Windows.Forms.Button();
+            this.addAvocados = new System.Windows.Forms.Button();
+            this.addMilk = new System.Windows.Forms.Button();
+            this.removeBananas = new System.Windows.Forms.Button();
+            this.removeApples = new System.Windows.Forms.Button();
+            this.removeOranges = new System.Windows.Forms.Button();
+            this.removeAvocados = new System.Windows.Forms.Button();
+            this.removeMilk = new System.Windows.Forms.Button();
+            this.addItemsToInventory = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.editButton = new System.Windows.Forms.Button();
+            this.reorderButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // viewButton
+            // 
+            this.viewButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.viewButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewButton.Location = new System.Drawing.Point(36, 127);
+            this.viewButton.Name = "viewButton";
+            this.viewButton.Size = new System.Drawing.Size(125, 75);
+            this.viewButton.TabIndex = 0;
+            this.viewButton.Text = "View Items";
+            this.viewButton.UseVisualStyleBackColor = true;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.searchButton.Location = new System.Drawing.Point(325, 127);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(128, 75);
+            this.searchButton.TabIndex = 1;
+            this.searchButton.Text = "Search Items";
+            this.searchButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(112, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "The Produce Store Inventory";
+            // 
+            // addBananas
+            // 
+            this.addBananas.AutoSize = true;
+            this.addBananas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addBananas.Location = new System.Drawing.Point(36, 329);
+            this.addBananas.Name = "addBananas";
+            this.addBananas.Size = new System.Drawing.Size(87, 32);
+            this.addBananas.TabIndex = 3;
+            this.addBananas.Text = "Bananas";
+            this.addBananas.UseVisualStyleBackColor = true;
+            // 
+            // addApples
+            // 
+            this.addApples.AutoSize = true;
+            this.addApples.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addApples.Location = new System.Drawing.Point(36, 384);
+            this.addApples.Name = "addApples";
+            this.addApples.Size = new System.Drawing.Size(87, 36);
+            this.addApples.TabIndex = 4;
+            this.addApples.Text = "Apples";
+            this.addApples.UseVisualStyleBackColor = true;
+            // 
+            // addOranges
+            // 
+            this.addOranges.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addOranges.Location = new System.Drawing.Point(36, 433);
+            this.addOranges.Name = "addOranges";
+            this.addOranges.Size = new System.Drawing.Size(87, 36);
+            this.addOranges.TabIndex = 5;
+            this.addOranges.Text = "Oranges";
+            this.addOranges.UseVisualStyleBackColor = true;
+            // 
+            // addAvocados
+            // 
+            this.addAvocados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addAvocados.Location = new System.Drawing.Point(36, 480);
+            this.addAvocados.Name = "addAvocados";
+            this.addAvocados.Size = new System.Drawing.Size(87, 33);
+            this.addAvocados.TabIndex = 6;
+            this.addAvocados.Text = "Avocados";
+            this.addAvocados.UseVisualStyleBackColor = true;
+            this.addAvocados.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // addMilk
+            // 
+            this.addMilk.AutoSize = true;
+            this.addMilk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addMilk.Location = new System.Drawing.Point(36, 525);
+            this.addMilk.Name = "addMilk";
+            this.addMilk.Size = new System.Drawing.Size(87, 38);
+            this.addMilk.TabIndex = 7;
+            this.addMilk.Text = "Milk";
+            this.addMilk.UseVisualStyleBackColor = true;
+            // 
+            // removeBananas
+            // 
+            this.removeBananas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeBananas.Location = new System.Drawing.Point(337, 329);
+            this.removeBananas.Name = "removeBananas";
+            this.removeBananas.Size = new System.Drawing.Size(87, 32);
+            this.removeBananas.TabIndex = 8;
+            this.removeBananas.Text = "Bananas";
+            this.removeBananas.UseVisualStyleBackColor = true;
+            // 
+            // removeApples
+            // 
+            this.removeApples.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeApples.Location = new System.Drawing.Point(337, 384);
+            this.removeApples.Name = "removeApples";
+            this.removeApples.Size = new System.Drawing.Size(87, 32);
+            this.removeApples.TabIndex = 9;
+            this.removeApples.Text = "Apples";
+            this.removeApples.UseVisualStyleBackColor = true;
+            // 
+            // removeOranges
+            // 
+            this.removeOranges.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeOranges.Location = new System.Drawing.Point(337, 433);
+            this.removeOranges.Name = "removeOranges";
+            this.removeOranges.Size = new System.Drawing.Size(87, 32);
+            this.removeOranges.TabIndex = 10;
+            this.removeOranges.Text = "Oranges";
+            this.removeOranges.UseVisualStyleBackColor = true;
+            // 
+            // removeAvocados
+            // 
+            this.removeAvocados.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeAvocados.Location = new System.Drawing.Point(337, 481);
+            this.removeAvocados.Name = "removeAvocados";
+            this.removeAvocados.Size = new System.Drawing.Size(87, 32);
+            this.removeAvocados.TabIndex = 11;
+            this.removeAvocados.Text = "Avocados";
+            this.removeAvocados.UseVisualStyleBackColor = true;
+            // 
+            // removeMilk
+            // 
+            this.removeMilk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.removeMilk.Location = new System.Drawing.Point(337, 528);
+            this.removeMilk.Name = "removeMilk";
+            this.removeMilk.Size = new System.Drawing.Size(87, 35);
+            this.removeMilk.TabIndex = 12;
+            this.removeMilk.Text = "Milk";
+            this.removeMilk.UseVisualStyleBackColor = true;
+            // 
+            // addItemsToInventory
+            // 
+            this.addItemsToInventory.AutoSize = true;
+            this.addItemsToInventory.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.addItemsToInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addItemsToInventory.Location = new System.Drawing.Point(36, 277);
+            this.addItemsToInventory.Name = "addItemsToInventory";
+            this.addItemsToInventory.Size = new System.Drawing.Size(118, 27);
+            this.addItemsToInventory.TabIndex = 13;
+            this.addItemsToInventory.Text = "Add Items";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(311, 277);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 27);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Remove Items";
+            // 
+            // editButton
+            // 
+            this.editButton.AutoSize = true;
+            this.editButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.editButton.Location = new System.Drawing.Point(26, 629);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(130, 30);
+            this.editButton.TabIndex = 15;
+            this.editButton.Text = "Edit Inventory";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // reorderButton
+            // 
+            this.reorderButton.AutoSize = true;
+            this.reorderButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reorderButton.Location = new System.Drawing.Point(337, 629);
+            this.reorderButton.Name = "reorderButton";
+            this.reorderButton.Size = new System.Drawing.Size(87, 30);
+            this.reorderButton.TabIndex = 16;
+            this.reorderButton.Text = "ReOrder";
+            this.reorderButton.UseVisualStyleBackColor = true;
+            // 
+            // milestoneProject
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 746);
+            this.Controls.Add(this.reorderButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.addItemsToInventory);
+            this.Controls.Add(this.removeMilk);
+            this.Controls.Add(this.removeAvocados);
+            this.Controls.Add(this.removeOranges);
+            this.Controls.Add(this.removeApples);
+            this.Controls.Add(this.removeBananas);
+            this.Controls.Add(this.addMilk);
+            this.Controls.Add(this.addAvocados);
+            this.Controls.Add(this.addOranges);
+            this.Controls.Add(this.addApples);
+            this.Controls.Add(this.addBananas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.viewButton);
+            this.Name = "milestoneProject";
+            this.Text = "Produce Inventory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button viewButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addBananas;
+        private System.Windows.Forms.Button addApples;
+        private System.Windows.Forms.Button addOranges;
+        private System.Windows.Forms.Button addAvocados;
+        private System.Windows.Forms.Button addMilk;
+        private System.Windows.Forms.Button removeBananas;
+        private System.Windows.Forms.Button removeApples;
+        private System.Windows.Forms.Button removeOranges;
+        private System.Windows.Forms.Button removeAvocados;
+        private System.Windows.Forms.Button removeMilk;
+        private System.Windows.Forms.Label addItemsToInventory;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button reorderButton;
+    }
+}
+
